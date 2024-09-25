c2656681935a63e6f610cdc954c18e1ef748cc00
RoLToNn1 <nazar.marchenko11@gmail.com>
2024-09-25 22:07:11 +0300

    add task_3.py

---
+++ codewars/python/task_3.py
@@ -0,0 +1,7 @@
+# Людина вибирає число, зробіть так щоб це число перетворилось в -1
+
+
+user_input = int(input("Input number -->\n"))
+while user_input > -1:
+    user_input =-1
+    print(user_input)
