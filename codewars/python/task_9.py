d05da94c2e8971327071433832e4c25912d1231e
RoLToNn1 <nazar.marchenko11@gmail.com>
2024-10-10 19:45:44 +0300

    add task_9.py

---
+++ codewars/python/task_9.py
@@ -0,0 +1,6 @@
+# Скільки вам буде років в 2099
+
+
+user_birthday_year = int(input("Put your birthday year -->\n"))
+years_old_on_2099 = 2099 - user_birthday_year
+print(f"In 2099 you will have: {years_old_on_2099}")
