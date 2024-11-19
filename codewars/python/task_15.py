245ca3f9944f8090822f2ce63b3317c4e98c6bc1
RoLToNn1 <nazar.marchenko11@gmail.com>
2024-11-19 23:15:27 +0200

    add task_15.py

---
+++ codewars/python/task_15.py
@@ -0,0 +1,10 @@
+# Вам надано числа написані в String вам потрібно перевести їх в intenger.
+my_string = ("86926057305777")
+my_string_1 = ("956093757674")
+my_string_2 = ("8590368737900")
+my_intenger = int(my_string)
+my_intenger_1 = int(my_string_1)
+my_intenger_2 = int(my_string_2)
+print(my_intenger)
+print(my_intenger_1)
+print(my_intenger_2)
